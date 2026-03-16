494f1ae18ac9ba137e5d9638d345f70143a2b62a
Frank Villanueva <frank05111996@gmail.com>
2026-03-16 09:23:47 -0500

    feat: Cambiar modo oscuro a 'class' en la configuración de Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 console.log("Using tailwind.config.js for web");
 export default {
-  darkMode: "media", // Usar clase 'dark' para activar dark mode
+  darkMode: "class", // Usar clase 'dark' para activar dark mode
   content: [
     "./src/index.html",
     "./src/preview.html",
